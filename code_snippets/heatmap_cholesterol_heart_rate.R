cf8efa51d42e1ca6ce07305a57c91f6e7c762357
Rutvi Patel <rutvipat0799@gmail.com>
2025-11-29 20:03:31 +0530

    heatmap_cholesterol_heart_rate

--- code_snippets/heatmap_cholesterol_heart_rate.R
+++ code_snippets/heatmap_cholesterol_heart_rate.R
@@ -1,1 +1,9 @@
-# heatmap code
+ggplot(data, aes(x=Cholesterol, y=Heart.Rate)) +
+stat_bin_2d(bins=30, aes(fill=..density..)) +
+scale_fill_gradient(low = "lightgreen", high = "red") +
+labs(
+title = "Heatmap: Cholesterol vs Heart Rate",
+x = "Cholesterol (mg/dL)",
+y = "Heart Rate (bpm)"
+) +
+theme_minimal(base_size = 14)
