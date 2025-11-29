aa97e2d3277d9993f3a37b988d8c33c065f312d6
Rutvi Patel <rutvipat0799@gmail.com>
2025-11-29 20:01:49 +0530

    smoke code

--- code_snippets/smoking_risk_plot.R
+++ code_snippets/smoking_risk_plot.R
@@ -1,1 +1,17 @@
-# smoking risk plot code
+ggplot(data, aes(x = factor(Smoking), fill = factor(Heart.Attack.Risk))) +
+geom_bar(position = "fill") +
+labs(title = "Proportion of Heart Attack Risk by Smoking Status",
+x = "Smoking (0 = No, 1 = Yes)",
+y = "Proportion",
+fill = "Risk") +
+scale_fill_manual(values = c("green", "red")) +
+theme_minimal(base_size = 14) +
+theme(
+axis.title.x = element_text(size =20),
+axis.title.y = element_text(size = 20),
+axis.text.x = element_text(size = 20),
+axis.text.y = element_text(size = 20),
+plot.title = element_text(size = 25, face = "bold"),
+legend.title = element_text(size = 12),
+legend.text = element_text(size = 10)
+)
